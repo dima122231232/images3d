7e532e0a0061aab6af2433a4183c232fa6ff6644
Dmytro <dimokmilok10@gmail.com>
2026-08-14 19:12:19 +0200

    add content

--- src/components/Statement/Statement.jsx
+++ src/components/Statement/Statement.jsx
@@ -1,8 +1,58 @@
+"use client";
+
 import "./Statement.css";
 
+import React, { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Statement() {
+    const section = useRef(null);
+
+    useGSAP((context) => {
+        const q = context.selector;
+        const isMobile = window.innerWidth < 800;
+
+        const wrappers = q(".statement__image-wrapper");
+        const images = q(".statement__image");
+
+        ScrollTrigger.create({
+            trigger: q(".statement__feature"),
+            start: "top top",
+            end: () => `+=${window.innerHeight * 3}px`,
+            pin: isMobile ? false : true,
+            pinSpacing: true,
+            scrub: 1,
+
+            onUpdate: (self) => {
+                const progress = self.progress;
+
+                const stageProgress = progress * wrappers.length;
+
+                wrappers.forEach((wrapper, index) => {
+                    const localProgress = gsap.utils.clamp(
+                        0,
+                        1,
+                        stageProgress - index
+                    );
+
+                    gsap.set(wrapper, {
+                        y: `${65 * (1 - localProgress)}%`,
+                        scale: 0.5 + (0.5 * localProgress),
+                    });
+
+                    gsap.set(images[index], {
+                        scale: 1.5 - (.5 * localProgress),
+                    });
+                });
+            }
+        });
+    }, { scope: section });
     return (
-        <section className="statement">
+        <section className="statement" ref={section}>
             <div className="container statement__container">
 
                 <div className="statement__content">
@@ -17,10 +67,21 @@ export default function Statement() {
 
                 <div className="statement__feature">
                     <div className="statement__images">
-                        <img className="statement__image" src="/statement/img1.png" alt="" />
-                        <img className="statement__image" src="/statement/img2.png" alt="" />
-                        <img className="statement__image" src="/statement/img3.png" alt="" />
-                        <img className="statement__image" src="/statement/img4.png" alt="" />
+                        <div className="statement__image-wrapper">
+                            <img className="statement__image" src="/statement/img1.png" alt="" />
+                        </div>
+
+                        <div className="statement__image-wrapper">
+                            <img className="statement__image" src="/statement/img2.png" alt="" />
+                        </div>
+
+                        <div className="statement__image-wrapper">
+                            <img className="statement__image" src="/statement/img3.png" alt="" />
+                        </div>
+
+                        <div className="statement__image-wrapper">
+                            <img className="statement__image" src="/statement/img4.png" alt="" />
+                        </div>
                     </div>
 
                     <div className="statement__info">
